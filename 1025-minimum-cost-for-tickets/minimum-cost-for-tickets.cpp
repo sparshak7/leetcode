38e57a7cb8bc21c5ef97033d63a98760820af99a
sparshak7 <96462023+sparshak7@users.noreply.github.com>
2024-07-16 03:24:06 +0530

    Time: 3 ms (58.96%) | Memory: 12.1 MB (39.95%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -13,11 +13,8 @@ public:
         int i = si;
         while(i<n && day[i]<day[si]+7) i++;
         
-        // for(i = si ; i<n and day[i]<day[si]+7 ; i++);
-        
         int cost_week = cost[1] + sub(day, cost, i, dp);
         
-        // for(i = si ; i<n and day[i]<day[si]+30 ; i++);
         i = si;
         while(i<n && day[i]<day[si]+30) i++;
         
@@ -26,9 +23,36 @@ public:
         return dp[si] = min({cost_day, cost_week , cost_month  });
     }
     
+    int tab(vector<int> &day, vector<int> &cost)
+    {
+        int n = day.size();
+        
+        vector<int> dp(n+1, 0);
+        
+        dp[n] = 0;
+        
+        for(int si=n-1;si>=0;si--) {
+            int cost_day = cost[0] + dp[si+1];
+        
+            int i = si;
+            while(i<n && day[i]<day[si]+7) i++;
+
+            int cost_week = cost[1] + dp[i];
+
+            i = si;
+            while(i<n && day[i]<day[si]+30) i++;
+
+            int cost_month = cost[2] + dp[i];  
+
+            dp[si] = min({cost_day, cost_week , cost_month  });
+        }
+        return dp[0];
+    }
+    
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
         vector<int> dp(n+1, -1);
-        return sub(days,costs,0,dp);
+        // return sub(days,costs,0,dp);
+        return tab(days, costs);
     }
 };
