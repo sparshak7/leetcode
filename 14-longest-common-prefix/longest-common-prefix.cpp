e669fa7899c2888b7b685b46bde78dd87fa7c831
sparshak7 <96462023+sparshak7@users.noreply.github.com>
2024-07-23 10:41:52 +0530

    Time: 3 ms (70.41%) | Memory: 11.1 MB (43.21%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,20 +1,33 @@
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+//         if (strs.size() == 0)
+//             return "";
+        
+//         sort(strs.begin(), strs.end());
+//         string first = strs[0];
+//         string last = strs[strs.size() - 1];
+//         int c = 0;
+//         while(c < first.size())
+//         {
+//             if (first.at(c) == last.at(c))
+//                 c++;
+//             else
+//                 break;
+//         }
+//         return c == 0 ? "" : first.substr(0, c);
+//     }
+// };
+
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        if (strs.size() == 0)
-            return "";
-        
-        sort(strs.begin(), strs.end());
-        string first = strs[0];
-        string last = strs[strs.size() - 1];
-        int c = 0;
-        while(c < first.size())
-        {
-            if (first.at(c) == last.at(c))
-                c++;
-            else
-                break;
+    string longestCommonPrefix(vector<string>& s) {
+        int ans = s[0].length(), n = s.size();
+        for(int i=1; i<n; i++){
+            int j = 0;
+            while(j<s[i].length() && s[i][j]==s[0][j])j++;
+            ans = min(ans, j);
         }
-        return c == 0 ? "" : first.substr(0, c);
+        return s[0].substr(0, ans);
     }
 };
